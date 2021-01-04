a3f2c6b55cb90f102a7152c1ecf9ed806fd3e35b
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2021-01-04 18:09:10 +0530

    Initial Commit

---
+++ SwiftUI-Buttons/SwiftUI_ButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ButtonsApp.swift
+//  SwiftUI-Buttons
+//
+//  Created by Pradyumn Shukla on 04/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
