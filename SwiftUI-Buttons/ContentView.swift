c645aa7dd1cd97a4efeff26c80d3fe3585c3f543
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2021-01-05 23:07:28 +0530

    Chapter 6 Completed ✌🏻

--- SwiftUI-Buttons/ContentView.swift
+++ SwiftUI-Buttons/ContentView.swift
@@ -9,27 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button (action: {
-            // What to perform
-            print("Delete button tapped!")
-        }, label: {
-            // What the button looks like
-            Label (
-                title: {  Text("Delete")
-                    .fontWeight(.semibold)
-                    .font(.title) },
-                icon: { Image(systemName: "trash")
-                    .font(.largeTitle) }
-            )
-            .padding()
-            .foregroundColor(.white)
-            .background(Color.red)
-            .clipShape(Capsule())
+        VStack {
+            Button (action: {
+                    // What to perform
+                    print("Delete button tapped!")
+                }, label: {
+                    // What the button looks like
+                    Label (
+                        title: {  Text("Delete")
+                            .fontWeight(.semibold)
+                            .font(.title) },
+                        icon: { Image(systemName: "trash")
+                            .font(.largeTitle) }
+                    )
+                })
+            .buttonStyle(GradientFullWidthButton())
             
-            
-                
-        })
+            Button (action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "plus")
+            })
+            .buttonStyle(RotationButtonStyle())
+        }
+        
+    }
+}
 
+struct GradientFullWidthButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding()
+            .foregroundColor(.primary)
+            .background(LinearGradient(gradient: Gradient(colors: [Color("DarkGreen"), Color("Green")]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+            .clipShape(Capsule())
+            .padding(.horizontal)
+            .shadow(radius: 10)
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
+    }
+}
+
+struct RotationButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .clipShape(Circle())
+            .rotationEffect(configuration.isPressed ? .degrees(45) : .zero)
+        
+            
     }
 }
 
